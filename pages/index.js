11b9991ff53ad54049094f98bf10455b173fdb89
amit-singh88598 <amitsingh992728@gmail.com>
2021-03-25 16:01:05 +0530

    changed layout of login page and added image on it

--- pages/index.js
+++ pages/index.js
@@ -3,34 +3,36 @@ import axios from "axios";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { useAuth } from "../auth";
+import { useAuth, UserProtectedPage } from "../auth";
 
 export default function Home() {
   const { isAuthenticatedUser } = useAuth();
   const router = useRouter();
-  useEffect(async () => {
-    // axios.defaults.headers = {
-    //   "Access-Control-Allow-Origin": "*",
-    //   "Content-Type": "application/json",
-    // };
-  }, []);
+  // useEffect(async () => {
+  //   // axios.defaults.headers = {
+  //   //   "Access-Control-Allow-Origin": "*",
+  //   //   "Content-Type": "application/json",
+  //   // };
+  // }, []);
   return (
     <div>
-      {isAuthenticatedUser == false ? (
+      {/* {isAuthenticatedUser == false ? (
         <div>
-          <Button onClick={() => router.push("/vendor/login")}>
+          <Button onClick={() => router.replace("/vendor/login")}>
             Login first
           </Button>
         </div>
-      ) : (
-        <div>
+      ) : ( */}
+      <div>
+        <UserProtectedPage>
           <Head>
             <title>Captain Shield</title>
             <link rel="icon" href="/favicon.ico" />
           </Head>
           <h5>Welcome in Captain Shield</h5>
-        </div>
-      )}
+        </UserProtectedPage>
+      </div>
+      {/* )} */}
     </div>
   );
 }
